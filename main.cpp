7b0ce05351e1f3ba3077176a98c5a8a8d76ea5e2
玄微 <admin@awaish.link>
2024-07-11 11:32:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 #define aa int main(){
 #define bb cout<<awa[0]<<endl;
-#define cc for(i=0;i<10;i++)cout<<awa[1]<<char(xwx[2]+i)<<char(xwx[2]+i)<<xwx[3]<<awa[15+i]<<endl;
+#define cc for(i=0;i<10;i++)cout<<awa[1]<<char(xwx[2]+i)<<char(xwx[2]+i)<<xwx[3]<<awa[16+i]<<endl;
 #define dd for(i=0;i<4;i++)cout<<awa[2+i]<<endl;
-#define ee for(i=0;i<6;i++)cout<<xwx[0]<<awa[i]<<xwx[0]<<xwx[1]<<endl;
+#define ee for(i=0;i<6;i++)cout<<xwx[0]<<awa[i]<<xwx[0]<<xwx[5]<<endl;
 #define ff for(i=0;i<10;i++)cout<<xwx[0]<<char(xwx[2]+i)<<char(xwx[2]+i)<<xwx[0]<<xwx[5];
-#define gg cout<<endl; for(i=0;i<10;i++)cout<<xwx[0]<<awa[15+i]<<xwx[0]<<xwx[1]<<endl;
-#define hh cout<<endl<<xwx[0]<<awa[25]<<xwx[0]<<xwx[4]<<xwx[1]<<endl;
+#define gg cout<<endl; for(i=0;i<10;i++)cout<<xwx[0]<<awa[16+i]<<xwx[0]<<xwx[5]<<endl;
+#define hh cout<<xwx[0]<<awa[26]<<xwx[0]<<xwx[4]<<xwx[1]<<endl;
 #define ii for(i=0;i<10;i++)cout<<char(xwx[2]+i)<<char(xwx[2]+i)<<endl;
 #define jj return 0;}
 using namespace std;
@@ -22,12 +22,12 @@ string awa[404]={
 "aa","bb","cc","dd","ee","ff","gg","hh","ii","jj",
 "int main(){",
 "cout<<awa[0]<<endl;",
-"for(i=0;i<10;i++)cout<<awa[1]<<char(xwx[2]+i)<<char(xwx[2]+i)<<xwx[3]<<awa[15+i]<<endl;",
+"for(i=0;i<10;i++)cout<<awa[1]<<char(xwx[2]+i)<<char(xwx[2]+i)<<xwx[3]<<awa[16+i]<<endl;",
 "for(i=0;i<4;i++)cout<<awa[2+i]<<endl;",
-"for(i=0;i<6;i++)cout<<xwx[0]<<awa[i]<<xwx[0]<<xwx[1]<<endl;",
+"for(i=0;i<6;i++)cout<<xwx[0]<<awa[i]<<xwx[0]<<xwx[5]<<endl;",
 "for(i=0;i<10;i++)cout<<xwx[0]<<char(xwx[2]+i)<<char(xwx[2]+i)<<xwx[0]<<xwx[5];",
-"cout<<endl; for(i=0;i<10;i++)cout<<xwx[0]<<awa[15+i]<<xwx[0]<<xwx[1]<<endl;",
-"cout<<endl<<xwx[0]<<awa[25]<<xwx[0]<<xwx[4]<<xwx[1]<<endl;",
+"cout<<endl; for(i=0;i<10;i++)cout<<xwx[0]<<awa[16+i]<<xwx[0]<<xwx[5]<<endl;",
+"cout<<endl<<xwx[0]<<awa[26]<<xwx[0]<<xwx[4]<<xwx[1]<<endl;",
 "for(i=0;i<10;i++)cout<<char(xwx[2]+i)<<char(xwx[2]+i)<<endl;",
 "return 0;}",
 "love Krs_"};
