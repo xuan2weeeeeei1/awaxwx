2780fe7a5a9ec26c86cf2bc7dbeca1ae4a18529a
玄微 <admin@awaish.link>
2024-07-11 11:34:45 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #define ee for(i=0;i<6;i++)cout<<xwx[0]<<awa[i]<<xwx[0]<<xwx[5]<<endl;
 #define ff for(i=0;i<10;i++)cout<<xwx[0]<<char(xwx[2]+i)<<char(xwx[2]+i)<<xwx[0]<<xwx[5];
 #define gg cout<<endl; for(i=0;i<10;i++)cout<<xwx[0]<<awa[16+i]<<xwx[0]<<xwx[5]<<endl;
-#define hh cout<<xwx[0]<<awa[26]<<xwx[0]<<xwx[4]<<xwx[1]<<endl;
+#define hh cout<<endl<<xwx[0]<<awa[26]<<xwx[0]<<xwx[4]<<xwx[1]<<endl;
 #define ii for(i=0;i<10;i++)cout<<char(xwx[2]+i)<<char(xwx[2]+i)<<endl;
 #define jj return 0;}
 using namespace std;
