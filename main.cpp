bccdb74e8ca47be8eb83d14b1358a98aa957e25f
玄微 <admin@awaish.link>
2024-07-11 11:27:20 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define aa int main(){
+#define bb cout<<awa[0]<<endl;
+#define cc for(i=0;i<10;i++)cout<<awa[1]<<char(xwx[2]+i)<<char(xwx[2]+i)<<xwx[3]<<awa[16+i]<<endl;
+#define dd for(i=0;i<4;i++)cout<<awa[2+i]<<endl;
+#define ee for(i=0;i<6;i++)cout<<xwx[0]<<awa[i]<<xwx[0]<<xwx[5]<<endl;
+#define ff for(i=0;i<10;i++)cout<<xwx[0]<<char(xwx[2]+i)<<char(xwx[2]+i)<<xwx[0]<<xwx[5];
+#define gg cout<<endl; for(i=0;i<10;i++)cout<<xwx[0]<<awa[16+i]<<xwx[0]<<xwx[5]<<endl;
+#define hh cout<<xwx[0]<<awa[26]<<xwx[0]<<xwx[4]<<xwx[1]<<endl;
+#define ii for(i=0;i<10;i++)cout<<char(xwx[2]+i)<<char(xwx[2]+i)<<endl;
+#define jj return 0;}
+using namespace std;
+
+char xwx[9]={34,59,97,32,125,44};int i;
+string awa[404]={
+"#include <bits/stdc++.h>",
+"#define ",
+"using namespace std;",
+"",
+"char xwx[9]={34,59,97,32,125,44};int i;",
+"string awa[404]={",
+"aa","bb","cc","dd","ee","ff","hh","gg","ii","jj",
+"int main(){",
+"cout<<awa[0]<<endl;",
+"for(i=0;i<10;i++)cout<<awa[1]<<char(xwx[2]+i)<<char(xwx[2]+i)<<xwx[3]<<awa[15+i]<<endl;",
+"for(i=0;i<4;i++)cout<<awa[2+i]<<endl;",
+"for(i=0;i<6;i++)cout<<xwx[0]<<awa[i]<<xwx[0]<<xwx[1]<<endl;",
+"for(i=0;i<10;i++)cout<<xwx[0]<<char(xwx[2]+i)<<char(xwx[2]+i)<<xwx[0]<<xwx[5];",
+"cout<<endl; for(i=0;i<10;i++)cout<<xwx[0]<<awa[15+i]<<xwx[0]<<xwx[1]<<endl;",
+"cout<<endl<<xwx[0]<<awa[25]<<xwx[0]<<xwx[4]<<xwx[1]<<endl;",
+"for(i=0;i<10;i++)cout<<char(xwx[2]+i)<<char(xwx[2]+i)<<endl;",
+"return 0;}",
+"love Krs_"};
+aa
+bb
+cc
+dd
+ee
+ff
+gg
+hh
+ii
+jj
